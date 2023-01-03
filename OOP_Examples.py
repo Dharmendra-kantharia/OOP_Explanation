0bc0e07be9fa9c4285cc196013e4e6dad82dc788
Dharmendra.kantharia <dharmendra.kantharia@gmail.com>
2023-01-03 23:42:09 +0530

    Example added

--- OOP_Examples.py
+++ OOP_Examples.py
@@ -1,12 +1,13 @@
 # Exercise 1 : Write a Python program to create a Vehicle class with max_speed and mileage instance attributes.
 class vehicle:
+    color = "White"
     def __init__(self,name,max_speed,mileage):
         self.name = name
         self.max_speed = max_speed
         self.mileage = mileage
 
     def seating_capacity(self,capacity):
-        return f"The seating capacity of a {self.name} is {capacity}passengers"
+        return f"The seating capacity of a {self.name} is {capacity} passengers"
 
 obj = vehicle("Student : Dharmendra",240,18)
 print(obj.name,obj.max_speed,obj.mileage)
@@ -25,6 +26,7 @@ print(obj1.name,obj1.max_speed,obj1.mileage)
 print(obj.seating_capacity(50))
 
 # Exercise 4: Class Inheritance
+#             Create a Bus class that inherits from the Vehicle class. Give the capacity argument of Bus.seating_capacity() a default value of 50.
 
 class Bus(vehicle):
     def seating_capacity(self,capacity = 55):
@@ -32,4 +34,82 @@ class Bus(vehicle):
 
 school_bus = Bus("School volvo",180,20)
 print(school_bus.seating_capacity())
+
+# Exercise 5: Define a property that must have the same value for every class instance (object)
+#             Define a class attribute”color” with a default value white. I.e., Every Vehicle should be white.
+
+class vehicle:
+    color = "White"
+    def __init__(self,name,max_speed,mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+class Bus(vehicle):
+    pass
+
+class Car(vehicle):
+    pass
+
+School_bus = Bus("School Volvo", 180, 12)
+print(School_bus.color, School_bus.name, "Speed:", School_bus.max_speed, "Mileage:", School_bus.mileage)
+
+car = Car("Audi Q5", 240, 18)
+print(car.color, car.name, "Speed:", car.max_speed, "Mileage:", car.mileage)
+
+# Exercise 6: Class Inheritance
+#             Create a Bus child class that inherits from the Vehicle class. The default fare charge of any vehicle is seating capacity * 100. 
+#             If Vehicle is Bus instance, we need to add an extra 10% on full fare as a maintenance charge. So total fare for bus instance will become 
+#             the final amount = total fare + 10% of the total fare.
+
+class vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+    def fare(self):
+        return self.capacity * 100
+
+class Bus(vehicle):
+    def fare(self):
+        amount = super().fare()
+        amount += amount * 10 / 100
+        return amount
+
+School_bus = Bus("School Volvo", 12, 50)
+print("Total Bus fare is:", School_bus.fare())
+
+# Exercise 7: Check type of an object
+#             Write a program to determine which class a given Bus object belongs to.
+
+class vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+class Bus(vehicle):
+    pass
+
+School_bus = Bus("School Volvo", 12, 50)
+
+# Python's built-in type()
+print(type(School_bus))
+
+# Exercise 8: Determine if School_bus is also an instance of the Vehicle class
+
+class vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+class Bus(vehicle):
+    pass
+
+School_bus = Bus("School Volvo", 12, 50)
+
+# Python's built-in isinstance() function
+print(isinstance(School_bus, vehicle))
 
